3917a7bfc71dc53c1a50537b05b5feece5c000ce
David Wu <david204code@gmail.com>
2020-09-24 18:20:39 +0100

    Add the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all 
+
+david = User.create(name: 'David', password: 'password')
+
+david.avatar.attach(
+  io: File.open('./public/avatars/3135715.png'),
+  filename: '3135715.png',
+  content_type: 'application/png'
+)
