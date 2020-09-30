fccac2101d10441ab2ccc1559325c1f7c8929c29
David Wu <david204code@gmail.com>
2020-09-30 12:53:31 +0100

    Add the user create, update action and direct_uploads_controller with its route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    user = User.create(name: params[:user][:name], password: params[:user][:password])
+    render json: user
   end
 
   def show 
@@ -14,14 +15,18 @@ class UsersController < ApplicationController
     avatar = rails_blob_path(user.avatar)
 
     if user.password == params[:password]
-      render json: {user:user, avatar: avatar}
+      render json: {user: user, avatar: avatar}
     else
       render json: { message: 'This user is not authenticated'}
     end
   end
 
   def update
-
+    user = User.find(params[:id])
+    user.update(avatar: params[:avatar])
+    avatar_url = rails_blob_path(user.avatar)
+    # byebug
+    render json: {user: user, avatar_url: avatar_url}
   end
 
 end
